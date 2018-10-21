f000cda98b03170e52935079007a606ebbf98f97
Natalie Halawani <hala1224@gmail.com>
2018-10-21 18:24:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function driversWithRevenueOver(driver, revenue){
    return driver.filter( x => x.revenue > revenue);
   
 }
+
+function driversNamesWithRevenueOver(driver, revenue){
+  
+  
+   return driver.filter.name ( x => x.revenue > revenue);
+  
+}
