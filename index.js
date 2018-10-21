7a7c71fe77733724825b1a71dc87f61e157a5ea0
Natalie Halawani <hala1224@gmail.com>
2018-10-21 21:08:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,3 +32,19 @@ function exactMatch(driver, attr) {
       return list=driver.filter(x => x.revenue===attr2);
    }
 }
+
+function exactMatchToList(driver, attr) {
+  
+  attr1=attr.name;
+  attr2=attr.revenue;
+  
+  
+    if(Object.keys(attr)=='name'){
+        return list1=driver.filter(x => x.name === attr1);
+    } 
+
+    if(Object.keys(attr) =='revenue'){
+      // console.log('inside if');
+      return list=driver.filter(x => x.revenue===attr2);
+   }
+}
